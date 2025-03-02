85c15f0fed74b797d4ae68510ebbf221c1d01783
lawj4 <jlaw0430@gmail.com>
2025-03-02 13:45:34 -0800

    confused

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lawj4.github.io/',  // Set to your repo name if using a project page
+  base: '/',  // Set to your repo name if using a project page
 });
